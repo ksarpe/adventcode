e7d4c2fc70fb48ceffef1bdbd1f561c71a6e820e
Kasper Janowski <2299kasper@gmail.com>
2023-12-15 23:51:54 +0100

    day5 not finished

--- day5/day5p2.py
+++ day5/day5p2.py
@@ -1,4 +1,4 @@
-file = "day5/day5.txt"
+file = "day5/day5p2.txt"
 
 flag = 1 # 1 -> open for new map, 0 -> new map forbidden
 seeds = []
@@ -6,52 +6,48 @@ mapping = []
 
 def remap():
     global seeds  # Use global keyword to modify the global variable
-    print("calling remap")
-    print("mapping:", mapping)
-    print("seeds:", seeds)
     for index, seed in enumerate(seeds):
-        print("checking seed:", seed)
         for i in range(len(mapping)):
-            print(f"checking in mapping: {mapping[i]}")
             if seed >= mapping[i][0] and seed <= mapping[i][1]:
-                print("seed before add:", seed)
                 seeds[index] += mapping[i][2]
-                print("seed after add:", seeds[index])
+
+def get_seeds(seeds_list):
+    global seeds
+
+    for seed in seeds_list:
+        if seed == "":
+            seeds_list.remove(seed)
+
+    current_seed = 0
+
+    for i, seed in enumerate(seed_list):
+        if (i+1) % 2 != 0:
+            current_seed = int(seed)
+        else:
+            seeds.append(current_seed)
+            seeds.append(current_seed + int(seed) - 1)
+    print(seeds)
 
 with open(file, "r") as f:
     for line in f:
         line = line.strip()
 
         split = line.split(":")
-        print("Current split:", split)
         # One timer, just to get seeds, the rest uses are unnecessary
         if split[0] == "seeds":
-            seeds_nr = split[1]
-            seeds_nr = seeds_nr.split(" ")
-            for nr in seeds_nr:
-                if nr == "":
-                    continue
-                seeds.append(int(nr))
-            continue
-        # getting next lines
-        # Current split might me:
-        # 0 -> blank line
-        # 2 -> new map indicator
-        # 3 -> numbers map     
-        # If we are now open for a new map then reset previous
+            seed_list = split[1].split(" ")
+            get_seeds(seed_list)
+
         if flag == 1:
             mapping = []
         current_split = line.split(" ")
         n = len(current_split)
         if n == 1:
-            print("Found a new line")
             if flag == 0:
-                print("Before new line there was mapping, calculating...")
                 remap()
             flag = 1
             continue
         elif n == 2:
-            print("Setting flag to 0, (busy)")
             flag = 0
             continue
         elif n == 3:
@@ -66,7 +62,7 @@ with open(file, "r") as f:
             temp_mapping.append(destination_start - source_start) # here we know how much to add/subtract from source numer to get destination
 
             mapping.append(temp_mapping)
-            
-print(seeds)
+
+print(seeds)         
 print(min(seeds))
         
